a7de44f042d98897985c17e183bdc589d02b93c8
Felipe Souza <felipelsouza.dev@gmail.com>
2023-04-09 15:11:13 -0300

    feat: update user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,11 +2,12 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Database from '@ioc:Adonis/Lucid/Database';
 import ClientException from 'App/Exceptions/ClientException';
 import User from 'App/Models/User';
-import CreateUser from 'App/Validators/CreateUserValidator';
+import CreateUserValidator from 'App/Validators/CreateUserValidator';
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator';
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
-    const userPayload = await request.validate(CreateUser);
+    const userPayload = await request.validate(CreateUserValidator);
 
     const { rows: hasUser } = await Database.rawQuery(
       `
@@ -41,5 +42,20 @@ export default class UsersController {
     const createdUser = await User.create(userPayload);
 
     return response.created(createdUser);
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { externalSource, name, email } = await request.validate(UpdateUserValidator);
+    const externalId = request.param('externalId');
+
+    const user = await User.query()
+      .where('external_id', externalId)
+      .andWhere('external_source', externalSource)
+      .andWhere('is_deleted', false)
+      .firstOrFail();
+
+    await user.merge({ name, email }).save();
+
+    return response.ok(user);
   }
 }
--- start/routes/user.ts
+++ start/routes/user.ts
@@ -1,3 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route';
 
 Route.post('/users', 'UsersController.store');
+Route.put('/users/:externalId', 'UsersController.update');
+
+Route.where('externalId', /^[a-zA-Z0-9-_]+$/);
