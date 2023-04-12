8b769e8f3d79b952ffe2ad20c80ceef38e5af218
Felipe Souza <felipelsouza.dev@gmail.com>
2023-04-11 22:11:14 -0300

    refactor: implement use cases in user controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import Database from '@ioc:Adonis/Lucid/Database';
-import ClientException from 'App/Exceptions/ClientException';
-import User from 'App/Models/User';
+import CreateUser from 'App/UseCases/User/Create';
+import UpdateUser from 'App/UseCases/User/Update';
 import CreateUserValidator from 'App/Validators/CreateUserValidator';
 import UpdateUserValidator from 'App/Validators/UpdateUserValidator';
 
@@ -9,53 +8,17 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = await request.validate(CreateUserValidator);
 
-    const { rows: hasUser } = await Database.rawQuery(
-      `
-        SELECT
-          1
-        FROM
-          users
-        WHERE
-          email = :email
-          AND is_deleted = false
-        UNION ALL
-        SELECT
-          1
-        FROM
-          users
-        WHERE
-          external_id = :externalId
-          AND external_source = :externalSource
-          AND is_deleted = false
-      `,
-      {
-        email: userPayload.email,
-        externalId: userPayload.externalId,
-        externalSource: userPayload.externalSource
-      }
-    );
-
-    if (hasUser && hasUser.length) {
-      throw new ClientException('users.alreadyExists', 409, 'CONFLICT__USER_ALREADY_EXISTS');
-    }
-
-    const createdUser = await User.create(userPayload);
+    const createdUser = await CreateUser.execute(userPayload);
 
     return response.created(createdUser);
   }
 
   public async update({ request, response }: HttpContextContract) {
-    const { externalSource, name, email } = await request.validate(UpdateUserValidator);
+    const userPayload = await request.validate(UpdateUserValidator);
     const externalId = request.param('externalId');
 
-    const user = await User.query()
-      .where('external_id', externalId)
-      .andWhere('external_source', externalSource)
-      .andWhere('is_deleted', false)
-      .firstOrFail();
+    const updatedUser = await UpdateUser.execute({ ...userPayload, externalId });
 
-    await user.merge({ name, email }).save();
-
-    return response.ok(user);
+    return response.ok(updatedUser);
   }
 }
